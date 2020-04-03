31da8bbadb0b9318118ff0fb383a546b57afc2df
dppatel522 <60637456+dppatel522@users.noreply.github.com>
2020-04-03 22:22:57 +0530

    Update CA1.c

--- CA1.c
+++ CA1.c
@@ -1,76 +1,146 @@
-#include<stdio.h>  
-
-void main()
-
+#include<stdio.h>
+#include<conio.h>
+int main()
 {
+  printf("\t\t\t----------------------- Scheduling -----------------------\n\n\n\n");
+  long int n,i=0,j=0;
+  printf("Enter Number of Processes : ");
+  scanf("%ld",&n ); 
+  double priority[n],avg_waiting,avg_turnaround,burstTime[n],arrivalTime[n],waitingTime[n],turnaroundTime[n], process[n], temp, completionTime[n],min,sum=0,sum2=0,wait_final, turnaround_final, wait_avg, turnaround_avg;
+  for(i=0;i<n;i++)
+  {
+    printf("\nEnter Burst Time for Process [%d] : ", i+1 );
+    scanf("%lf", &burstTime[i]);
+    printf("Enter Arrival Time for Process [%d] : ", i+1 );
+    scanf("%lf", &arrivalTime[i] );
+    process[i]=i+1;
+  }
 
-   int bt[20],p[20],wt[20],tat[20],i,j,n,total=0,pos,temp;
+  printf("\n\n\t\t\t -------------- Entered Values are --------------\n\n");
+  printf("\t\t\t---------------------------------------\n");
+  printf("\t\t\t| Process | Arrival Time | Burst Time |\n");
+  printf("\t\t\t---------------------------------------\n");
+  for(i=0;i<n;i++)
+  {
+    printf("\t\t\t|  P[%0.0lf]   |       %0.0lf     |     %0.0lf     |\n",process[i],arrivalTime[i],burstTime[i]);
+  }
+    printf("\t\t\t---------------------------------------\n");
 
-   float avg_wt,avg_tat;
 
-   printf("Enter number of process:");
+  printf("\n\n\t\t\t-------- Sorting Processes according to Arrivaltime --------\n");
 
-   scanf("%d",&n);  
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<n;j++)
+    {
+      if(arrivalTime[i]<arrivalTime[j])
+      {
+        
+        temp = burstTime[j];
+        burstTime[j] = burstTime[i];
+        burstTime [i] = temp;
+	
+	      temp = process[j];
+        process[j] = process[i];
+        process[i] = temp;
 
-   printf("\nEnter Estimated Time:\n");
+	      temp = arrivalTime[j];
+        arrivalTime[j] = arrivalTime[i];
+        arrivalTime[i] = temp;
+      
+      }
+    }
+  }
+  printf("\n\n\t\t\t -------------- Now Values are --------------\n\n");
+  printf("\t\t\t---------------------------------------\n");
+  printf("\t\t\t| Process | Arrival Time | Burst Time |\n");
+  printf("\t\t\t---------------------------------------\n");
+  for(i=0;i<n;i++)
+  {
+    printf("\t\t\t|  P[%0.0lf]   |       %0.0lf     |     %0.0lf     |\n",process[i],arrivalTime[i],burstTime[i]);
+  }
+    printf("\t\t\t---------------------------------------\n");
 
-   for(i=0;i<n;i++)
 
-   {
+  long int k = 1;
+  double b_time = 0;
+  for(j=0;j<n;j++)
+  {
+    b_time = b_time + burstTime[j];
+    min = burstTime[k];
 
-       printf("p%d:",i+1);
+    for(i=k;i<n;i++)
+    {
+      if((b_time >= arrivalTime[i])&&(burstTime[i]<min))
+      {
+        temp = burstTime[k];
+        burstTime[k] = burstTime[i];
+        burstTime[i] = temp;
 
-       scanf("%d",&bt[i]);
+        temp = arrivalTime[k];
+        arrivalTime[k] = arrivalTime[i];
+        arrivalTime[i] = temp;
 
-       p[i]=i+1;           //contains process number
+        temp = process[k];
+        process[k] = process[i];
+        process[i] = temp;
+      }
+    }
+    k++;
+  }
+  waitingTime[0] = 0;
+  for(i=1;i<n;i++)
+  {
+    sum += burstTime[i-1];
+    waitingTime[i] = sum - arrivalTime[i];
+    wait_final += waitingTime[i]; 
+  }
+  wait_avg = wait_final/n;
+  for(i=0;i<n;i++)
+  {
+    sum2 += burstTime[i];
+    turnaroundTime[i] = sum2 - arrivalTime[i];
+    turnaround_final += turnaroundTime[i];
+  }
+  turnaround_avg=turnaround_final/n;
+printf("\n\n\t\t\t -------------- Now Values are --------------\n\n");
+  printf("\t\t\t-----------------------------------------------------------------------------\n");
+  printf("\t\t\t| Process | Arrival Time | Burst Time |  Waiting Time  |  Turn Around Time  |\n");
+  printf("\t\t\t-----------------------------------------------------------------------------\n");
+  for(i=0;i<n;i++)
+  {
+    printf("\t\t\t|  P[%0.0lf]   |       %0.0lf      |     %0.0lf      |        %0.0lf       |         %0.0lf          |\n",process[i],arrivalTime[i],burstTime[i],waitingTime[i],turnaroundTime[i]);
+  }
+    printf("\t\t\t-----------------------------------------------------------------------------\n");
 
-   }  
+  
+  completionTime[0] = burstTime[0];
+  for(i=1;i<n;i++)
+  {
+    completionTime[i] = completionTime[i-1] + burstTime[i];
+  }
 
-   //sorting burst time in ascending order using selection sort
+  for(i=0;i<n;i++)
+  {
+    priority[i] = 1+waitingTime[i]/completionTime[i];
+    printf("%lf\n",priority[i]);
+  }
 
-   for(i=0;i<n;i++)
-        {
 
-       pos=i;
+  printf("\n\n\t\t\t -------------- Final Values are --------------\n\n");
+  printf("\t\t\t-----------------------------------------------------------------------------\n");
+  printf("\t\t\t| Process | Arrival Time | Burst Time |  Waiting Time  |  Turn Around Time  |\n");
+  printf("\t\t\t-----------------------------------------------------------------------------\n");
+  printf("\t\t\t|  P[%0.0lf]   |       %0.0lf      |     %0.0lf      |        %0.0lf       |         %0.0lf          |\n",process[0],arrivalTime[0],burstTime[0],waitingTime[0],turnaroundTime[0]);
+  for(i=n-1;i>0;i--)
+  {
+    printf("\t\t\t|  P[%0.0lf]   |       %0.0lf      |     %0.0lf      |        %0.0lf       |         %0.0lf          |\n",process[i],arrivalTime[i],burstTime[i],waitingTime[i],turnaroundTime[i]);
+  }
+    printf("\t\t\t-----------------------------------------------------------------------------\n");
 
-       for(j=i+1;j<n;j++)
-
-       {
-
-           if(bt[j]<bt[pos])
-
-               pos=j;
-
-       }  
-
-       temp=bt[i];
-
-       bt[i]=bt[pos];
-
-       bt[pos]=temp;  
-
-       temp=p[i];
-
-       p[i]=p[pos];
-
-       p[pos]=temp;
-
-   }  
-
-   wt[0]=0; //waiting time for first process will be zero  
-
-   //calculate waiting time
-
-   for(i=1;i<n;i++)
-
-   {
-
-       wt[i]=0;
-
-       for(j=0;j<i;j++)
-
-           wt[i]+=bt[j];  
-
-       total+=wt[i];
-
-   }  
+  printf("\n\n\n\t\t\tAverage Turn Around Time : %lf",turnaround_avg);
+  printf("\n\t\t\tAverage Waiting Time     : %lf\n\n",wait_avg);
+	
+  getch();
+  return 0;
+}
